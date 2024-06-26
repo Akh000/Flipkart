36f910d6abf6c21c9b47fdfd167d0e59bc9d720d
Akh000 <akhmangalkar2010@gmail.com>
2024-06-26 16:07:20 +0530

    fifth commit

--- testcases/test_seller_page.py
+++ testcases/test_seller_page.py
@@ -5,12 +5,12 @@ from pageObject.seller_category import Seller_Page
 
 
 class Test_Seller:
-    link = Readconfig.getUrl()
+    links = Readconfig.getUrl()
 
     @pytest.mark.seller
     def test_seller_login(self, setup):
         self.driver = setup
-        self.driver.get(self.link)
+        self.driver.get(self.links)
         self.sel = Seller_Page(self.driver)
         self.sel.click_on_become_seller()
         self.sel.click_on_login_button()
